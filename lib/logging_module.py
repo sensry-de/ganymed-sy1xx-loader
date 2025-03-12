8f71b95c8397d9f4da1d40b32deb0119a20709ad
SG <s.ginka@sensry.de>
2025-03-12 18:02:37 +0100

    mod: anpassung an PyPI lib

--- lib/logging_module.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s\t\t%(levelname)s\t\t%(message)s")
-logger = logging.getLogger()
-# logger.addHandler(logging.FileHandler('rocket_launcher.log', 'a'))
--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import os
-from lib.flash import Flash
+from sry_gnm_sy1xx_bl import SryGnmSy1xxBL
 
+# define the file to be uploaded
 coreguard_bin = os.path.join("bin", "coreguard-bl.bin")
 application_bin = os.path.join("bin", "zephyr_demo_app.bin")
 
-flash = Flash()
+# create the loader
+flash = SryGnmSy1xxBL()
 
+# connect to serial
 flash.connect("/dev/ttyUSB0")
 
 # convert binary to application ganymed image
 application_gnm = flash.convert_zephyr_bin(application_bin)
 
+# set the controller into bootloader mode
 flash.enter_loading_mode()
 
+# clear the internal flash
 flash.clear_mram()
 
+# write the new binaries
 flash.write_mram(coreguard_bin, application_gnm)
+
+print("done")
